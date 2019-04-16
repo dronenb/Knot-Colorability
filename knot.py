504cc20ab289bff3a9dfb45b27ce11cc9823bdcd
Ben Dronen <benjamin.dronen@gmail.com>
2019-04-16 11:33:55 -0400

    Added a bunch of SVG's, made some progress on the knot table scene.

--- knot.py
+++ knot.py
@@ -11,10 +11,38 @@ class Knot(SVGMobject):
 		except:
 			warnings.warn("No SVG file for knot: %s" % mode)
 			exit()
-class SVGKnot(Scene):
+class KnotTable(Scene):
 	def construct(self):
-		trefoil = Knot("5_1")
-		self.play(DrawBorderThenFill(trefoil))
-		colored_trefoil = Knot("5_1");
-		colored_trefoil.submobjects[0].set_fill(RED, opacity = 1)
-		self.play(FadeIn(colored_trefoil))
+		title_text = TextMobject("Some Standard Knots")
+		title_text.to_edge(UP)
+		self.play(FadeIn(title_text))
+#		unknot		= Knot()
+		knot_3_1	= Knot("3_1")
+		knot_3_1.move_to(LEFT * 5 + UP)
+		knot_4_1	= Knot("4_1")
+		knot_4_1.move_to(LEFT * 2.5 + UP)
+		knot_5_1	= Knot("5_1")
+		knot_5_1.move_to(UP)
+		knot_5_2	= Knot("5_2")
+		knot_5_2.move_to(RIGHT * 2.5 + UP)
+		knot_6_1	= Knot("6_1")
+		knot_6_1.move_to(UP + RIGHT * 5)
+		knot_6_2	= Knot("6_2")
+		knot_6_2.move_to(DOWN * 2 + LEFT * 5)
+		knot_6_3	= Knot("6_3")
+		knot_6_3.move_to(DOWN * 2 + LEFT * 2.5)
+		knot_7_1	= Knot("7_1")
+		knot_7_1.move_to(DOWN * 2)
+		knot_7_2	= Knot("7_2")
+		knot_7_2.move_to(DOWN * 2 + RIGHT * 2.5)
+		self.play(
+			FadeIn(knot_3_1),
+			FadeIn(knot_4_1),
+			FadeIn(knot_5_1),
+			FadeIn(knot_5_2),
+			FadeIn(knot_6_1),
+			FadeIn(knot_6_2),
+			FadeIn(knot_6_3),
+			FadeIn(knot_7_1),
+			FadeIn(knot_7_2),
+		)
--- make.py
+++ make.py
@@ -2,10 +2,14 @@
 import os
 import sys
 scenes = [
-	'SVGKnot',
+	'KnotTable',
 ]
 
 manim_path = os.path.join(os.path.dirname(os.path.abspath( __file__ )), 'manim', 'manim.py')
 
 for scene in scenes:
-	os.system("python3 %s %s %s" % (manim_path, "knot.py", scene))
+	command = "python3 %s %s %s" % (manim_path, "knot.py", scene)
+	if len(sys.argv) > 1:
+		for i in range(1, len(sys.argv)):
+			command = command + " " + sys.argv[i]
+	os.system(command)
