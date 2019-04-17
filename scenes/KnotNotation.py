ceb701830d8e520c7c5079cd1a3fc57dd1143714
Jonathan Swerdlow <swerdloj@andrews.edu>
2019-04-17 14:53:27 -0400

    Knot notation scene

---
+++ scenes/KnotNotation.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath( __file__ )), '..'))
+
+from knot import *
+
+class KnotNotation(KnotScene):
+    def construct(self):
+        basic_knot = Knot("3_1")
+        # basic_knot.textMobject.set_color_by_tex_color_map()
+        scene_title = TextMobject("Knot Notation")
+        scene_title.to_edge(UP)
+
+        self.play(FadeIn(scene_title))
+        self.play(*DrawKnot(basic_knot))
+        self.play(FadeIn(basic_knot.textMobject))
+        self.play(ApplyMethod(basic_knot.shift, LEFT * 6), ApplyMethod(basic_knot.textMobject.shift, LEFT * 6))
+
+        sup_l1 = TextMobject("Crossing Number: ")
+        sup_l1.set_color(BLUE)
+
+        sup_l2 = TextMobject("Minimum number of crossings")
+        sup_l2.next_to(sup_l1, DOWN)
+
+        sup_l3 = TextMobject("given any knot diagram")
+        sup_l3.next_to(sup_l2, DOWN)
+
+        self.play(FadeIn(sup_l1))
+        self.play(FadeIn(sup_l2), FadeIn(sup_l3))
+        self.play(FadeOut(sup_l1), FadeOut(sup_l2), FadeOut(sup_l3))
+
+        sub_l1 = TextMobject("Knot Index:")
+        sub_l2 = TextMobject("Arbitrary index assigned to specific")
+        sub_l2.next_to(sub_l1, DOWN)
+        sub_l3 = TextMobject("knot of same crossing number")
+        sub_l3.next_to(sub_l2, DOWN)
+
+        self.play(FadeIn(sub_l1))
+        self.play(FadeIn(sub_l2), FadeIn(sub_l3))
+        self.play(FadeOut(basic_knot), FadeOut(sub_l1), FadeOut(sub_l2), FadeOut(sub_l3))
