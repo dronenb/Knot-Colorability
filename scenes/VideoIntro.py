c498bdd72c7be7b6c3a558a6c76081dae787b3a6
Jonathan Swerdlow <swerdloj@andrews.edu>
2019-04-17 19:51:26 -0400

    Commented VideoIntro

--- scenes/VideoIntro.py
+++ scenes/VideoIntro.py
@@ -1,13 +1,12 @@
 """
 Video starting sequence
+AU Logo first?
 """
 
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath( __file__ )), '..'))
 from knot import *
-
-
 
 names = {
     'bd': TextMobject("Ben Dronen"),
@@ -19,13 +18,15 @@ title = TextMobject("Knot Colorability")
 
 class VideoIntro(Scene):
     def construct(self):
-        # Initialize the scene
+        # Keep title center
         title.scale(2)
+        # Place names below title
         for name in names:
             names[name].next_to(title, DOWN)
         names['bd'].shift(LEFT * 5)
         names['js'].shift(RIGHT * 5)
 
+        # Add space between names and title
         title.shift(UP * 1)
 
         self.play(FadeIn(title))
