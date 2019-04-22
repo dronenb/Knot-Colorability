880e7d9c53d5b9ed036a9361797a1bd1ef1de4b8
Jonathan Swerdlow <swerdloj@andrews.edu>
2019-04-22 12:15:47 -0400

    Added unknot example knot scene

---
+++ scenes/KnotExample.py
@@ -0,0 +1,28 @@
+"""
+Unknot to Trefoil
+"""
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+from knot import *
+
+class KnotExample(KnotScene):
+    def construct(self):
+        # Load knots
+        unknot = Circle(color=WHITE, radius=2, stroke=10) # Does sroke do anything?
+        unknot_text = TextMobject("Unknot")
+        unknot_text.next_to(unknot, DOWN)
+
+        """
+        trefoil = Knot("3_1")
+        trefoil_text = TexMobject(r"\text{Knot } 3_1")
+        trefoil_text.next_to(trefoil, DOWN)
+        """
+        self.play(DrawBorderThenFill(unknot))
+        self.play(FadeIn(unknot_text))
+        """
+        self.play(Transform(unknot, trefoil), Transform(unknot_text, trefoil_text))
+        """
+
+        self.play(FadeOut(VGroup(*self.get_mobjects())))
